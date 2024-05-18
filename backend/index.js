87da09cd00c16965a6129dd226a5ff0081d2c344
atiktest2023 <contact.atikurrahman@gmail.com>
2024-05-18 14:21:22 +0600

    ok

--- backend/index.js
+++ backend/index.js
@@ -85,7 +85,7 @@ app.post("/video-info", async (req, res) => {
 
 app.get("/get-pc", async (req, res) => {
   try {
-    const pcData = await AllTime.find({}, undefined, { sort: { 'lasttime': -1 } });
+    const pcData = await AllTime.find({});
     res.json(pcData);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -95,7 +95,7 @@ app.get("/get-pc", async (req, res) => {
 // Define the 'get-video' endpoint
 app.get("/get-video", async (req, res) => {
   try {
-    const videoData = await VideoInfo.find({}, undefined, { sort: { video_end_date_time: -1 } });
+    const videoData = await VideoInfo.find({});
     res.json(videoData);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -103,7 +103,7 @@ app.get("/get-video", async (req, res) => {
 });
 app.get("/get-interval", async (req, res) => {
   try {
-    const videoData = await IntervalInfo.find({}, undefined, { sort: { lasttime: -1 } });
+    const videoData = await IntervalInfo.find({});
     res.json(videoData);
   } catch (err) {
     res.status(500).json({ message: err.message });
