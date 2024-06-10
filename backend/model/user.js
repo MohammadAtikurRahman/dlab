562c91b18a0802cc8697e12ebd27cd183b7865dd
atiktest2023 <contact.atikurrahman@gmail.com>
2024-06-10 12:47:09 +0600

    indexing

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,58 +1,8 @@
-// const mongoose = require("mongoose");
-
-// const allTimeSchema = new mongoose.Schema({
-//   dayid: Number,
- 
-//   starttime: String,
-//   totaltime: Number,
-//   lasttime: String,
-//   pcname: String,
-//   eiin: Number,
-//   schoolname: String,
-//   labnum: Number,
-//   pcnum: Number,
-// });
-
-// const allVideoSchema = new mongoose.Schema({
-//   dayid: Number,
- 
-//   pcname: String,
-//   eiin: Number,
-//   schoolname: String,
-//   labnum: Number,
-//   pcnum: Number,
-//   video_name: String,
-//   video_start: String,
-//   video_start_date_time: String,
-//   video_end: String,
-//   video_end_date_time: String,
-//   duration: Number,
-// });
-
-
-// const interValSchema = new mongoose.Schema({
-//   dayid: Number,
- 
-//   starttime: String,
-//   totaltime: Number,
-//   lasttime: String,
-//   pcname: String,
-//   eiin: Number,
-//   schoolname: String,
-//   labnum: Number,
-//   pcnum: Number,
-// });
-
-
-// module.exports = {
-//   AllTime: mongoose.model("AllTime", allTimeSchema),
-//   VideoInfo: mongoose.model("VideoInfo", allVideoSchema),
-//   IntervalInfo: mongoose.model("Intervalinfo",interValSchema)
-// };
 const mongoose = require("mongoose");
 
 const allTimeSchema = new mongoose.Schema({
   dayid: Number,
+ 
   starttime: String,
   totaltime: Number,
   lasttime: String,
@@ -62,11 +12,10 @@ const allTimeSchema = new mongoose.Schema({
   labnum: Number,
   pcnum: Number,
 });
-allTimeSchema.index({ dayid: 1 });
-allTimeSchema.index({ eiin: 1, labnum: 1, pcnum: 1 });
 
 const allVideoSchema = new mongoose.Schema({
   dayid: Number,
+ 
   pcname: String,
   eiin: Number,
   schoolname: String,
@@ -79,11 +28,11 @@ const allVideoSchema = new mongoose.Schema({
   video_end_date_time: String,
   duration: Number,
 });
-allVideoSchema.index({ eiin: 1, labnum: 1, pcnum: 1 });
-allVideoSchema.index({ video_name: 1 });
+
 
 const interValSchema = new mongoose.Schema({
   dayid: Number,
+ 
   starttime: String,
   totaltime: Number,
   lasttime: String,
@@ -93,11 +42,10 @@ const interValSchema = new mongoose.Schema({
   labnum: Number,
   pcnum: Number,
 });
-interValSchema.index({ dayid: 1 });
-interValSchema.index({ eiin: 1, labnum: 1, pcnum: 1 });
+
 
 module.exports = {
   AllTime: mongoose.model("AllTime", allTimeSchema),
   VideoInfo: mongoose.model("VideoInfo", allVideoSchema),
-  IntervalInfo: mongoose.model("Intervalinfo", interValSchema),
+  IntervalInfo: mongoose.model("Intervalinfo",interValSchema)
 };
