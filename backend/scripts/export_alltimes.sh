9dc1c9544e30437db51565bedb48bd87c75ec42a
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-06-12 12:30:00 +0600

    created some scripts for exporting collections

---
+++ backend/scripts/export_alltimes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+timestamp=$(date +"%Y%m%d_%H%M%S")
+filename="alltimes_$timestamp"
+mongoexport --collection=alltimes --db=dlab --type=csv --fields=dayid,starttime,totaltime,lasttime,pcname,eiin,schoolname,labnum,pcnum --out "$filename.csv"
+zip "${filename}.zip" "${filename}.csv"
+
+mv "$filename.zip" ../static
