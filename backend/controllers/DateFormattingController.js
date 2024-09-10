04d99d0b599ec215d52c117a76981a232d8f41e2
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-09-10 19:39:51 +0600

    interval update

--- backend/controllers/DateFormattingController.js
+++ backend/controllers/DateFormattingController.js
@@ -12,10 +12,27 @@ class DateFormattingController {
   }
   async formatDate(req, res) {
     try {
-      const alltimes = await AllTime.find({}, {starttime: 1, lasttime: 1}).exec();
-      //const intervals = await IntervalInfo.find({}, {starttime: 1, lasttime: 1}).exec();
+      //const alltimes = await AllTime.find({}, {starttime: 1, lasttime: 1}).exec();
+      const intervals = await IntervalInfo.find({}, {starttime: 1, lasttime: 1}).exec();
 
-      let bulkOps = alltimes.map((time) => ({
+      // let bulkOps = alltimes.map((time) => ({
+      //   updateOne: {
+      //     filter: {
+      //       _id: time._id
+      //     },
+      //     update: {
+      //       $set: {
+      //         starttime: this.convertToISO(time.starttime),
+      //         lasttime: this.convertToISO(time.lasttime),
+      //       }
+      //     },
+      //   }
+      // }))
+
+      // let savedTimes = await AllTime.bulkWrite(bulkOps);
+      // let alltimesUpdated = savedTimes.length;
+
+      bulkOps = intervals.map((time) => ({
         updateOne: {
           filter: {
             _id: time._id
@@ -25,33 +42,16 @@ class DateFormattingController {
               starttime: this.convertToISO(time.starttime),
               lasttime: this.convertToISO(time.lasttime),
             }
-          },
+          }
         }
       }))
 
-      let savedTimes = await AllTime.bulkWrite(bulkOps);
-      let alltimesUpdated = savedTimes.length;
-
-      //       bulkOps = intervals.map((time) => ({
-      //         updateOne: {
-      //           filter: {
-      //             _id: time._id
-      //           },
-      //           update: {
-      //             $set: {
-      //               starttime: this.convertToISO(time.starttime),
-      //               lasttime: this.convertToISO(time.lasttime),
-      //             }
-      //           }
-      //         }
-      //       }))
-
-      //       savedTimes = await IntervalInfo.bulkWrite(bulkOps);
-      //       let intervalUpdated = savedTimes.length;
+      savedTimes = await IntervalInfo.bulkWrite(bulkOps);
+      let intervalUpdated = savedTimes.length;
 
       return res.status(200).json({
-        alltimesUpdated,
-        //intervalUpdated
+        //alltimesUpdated,
+        intervalUpdated
       });
     } catch (error) {
       return res.status(500).json({error: error.message});
